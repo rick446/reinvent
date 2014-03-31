5f502f968b71c0d258effd8d3f97201e02be64b4
Rick Copeland <rick@synapp.io>
2014-03-31 11:24:22 -0400

    Add post archiving and index page generation
    Signed-off-by: Rick Copeland <rick@synapp.io>

--- reinvent/script.py
+++ reinvent/script.py
@@ -2,6 +2,9 @@ import os
 import glob
 import logging
 import logging.config
+from itertools import groupby
+from datetime import datetime
+from collections import defaultdict
 
 import yaml
 import markdown
@@ -32,51 +35,112 @@ def remake_blog():
     logging.config.dictConfig(config['logging'])
     log = logging.getLogger(__name__)
     log.info('Logging configured')
+
+    # First, gather the posts
     posts = []
-    for fn_content in glob.glob('posts/*.md'):
-        slug = os.path.basename(fn_content).rsplit('.', 1)[0]
-        entry = render_page(
+    for fn_post in glob.glob('posts/*.md'):
+        slug = os.path.basename(fn_post).rsplit('.', 1)[0]
+        md = markdown.Markdown(extensions=['meta'])
+        md_pre = markdown.Markdown()
+        with open(fn_post) as fp:
+            md_content = fp.read()
+            html_content = md.convert(md_content)
+            html_preview = md_pre.convert(
+                text.truncate(md_content, 300, whole_word=True))
+        post_data = dict(
+            fn=fn_post,
+            slug=slug,
+            path='posts/{}.html'.format(slug),
+            title=md.Meta['title'][0],
+            md_content=md_content,
+            html_content=html_content,
+            html_preview=html_preview,
+            meta=md.Meta,
+            date=datetime.strptime(md.Meta['date'][0], '%Y-%m-%d'))
+        if eval(md.Meta['published'][0]):
+            posts.append(Object.ify(post_data))
+
+    posts.sort(key=lambda p: p.date, reverse=True)
+    archive = make_archive(posts)
+
+    # Generate post html
+    for post in posts:
+        render_page(
             config,
-            fn_out='public/posts/{}.html'.format(slug),
+            fn_out='public/posts/{}.html'.format(post.slug),
             fn_template='post.html',
-            fn_content=fn_content,
-            blog=config['blog'],
-            h=helpers)
-        if eval(entry.meta.published[0]):
-            posts.append(Object.ify(entry))
-    posts.sort(key=lambda p: p.meta.date, reverse=True)
+            page_id='blog',
+            archive=archive,
+            **post)
+
+    # Generate archive pages
+    for month_data in archive:
+        render_page(
+            config,
+            fn_template='archive.html',
+            page_id='blog',
+            archive=archive,
+            **month_data)
+
+    # Generate other pages
     for slug, pg in config['pages'].items():
+        if 'content' in pg:
+            with open(pg['content']) as fp:
+                md_content = fp.read()
+                html_content = markdown.markdown(md_content)
+        else:
+            html_content = ''
         render_page(
             config,
             fn_out='public/{}.html'.format(slug),
             fn_template=pg.get('template', 'page.html'),
-            fn_content=pg.get('content', None),
+            html_content=html_content,
+            title=pg['title'],
             page=pg,
-            blog=config['blog'],
-            h=helpers,
-            posts=posts)
+            page_id=slug,
+            posts=posts,
+            archive=archive)
 
-def render_page(config, fn_out, fn_template, fn_content, **kwargs):
-    path = '/' + '/'.join(fn_out.split('/')[1:])
-    md = markdown.Markdown(extensions=['meta'])
-    md_pre = markdown.Markdown(extensions=['meta'])
-    Template = loader.import_(fn_template)
-    if fn_content is not None:
-        with open(fn_content) as fp:
-            md_content = fp.read()
-        content = md.convert(md_content)
-        preview = md_pre.convert(text.truncate(md_content, 300, whole_word=True))
+def render_page(config, fn_out, fn_template, **kwargs):
+    log.info('Render %s', fn_out)
+    if 'path' in kwargs:
+        path = kwargs.pop('path')
     else:
-        content = preview = ''
+        path = '/' + '/'.join(fn_out.split('/')[1:])
+    Template = loader.import_(fn_template)
     with open(fn_out, 'w') as fp:
         context = dict(
-            path=path, content=content, preview=preview, **kwargs)
-        if hasattr(md, 'Meta'):
-            context['meta'] = md.Meta
+            path=path,
+            h=helpers,
+            blog=config['blog'],
+            **kwargs)
         context = Object.ify(context)
         for chunk in Template(context):
             fp.write(chunk)
     return context
+
+def make_archive(posts):
+    '''Group posts by month, generating archive data'''
+    posts_by_month = defaultdict(list)
+    for post in posts:
+        dt = post.date
+        posts_by_month[dt.replace(day=1)].append(post)
+
+    archive = []
+    for month, month_posts in sorted(posts_by_month.items(), reverse=True):
+        path='/archive-{:%Y-%m}.html'.format(month)
+        fn_out = 'public/' + path
+        s_month = '{:%B %Y}'.format(month)
+        archive_data = Object.ify(dict(
+            s_month=s_month,
+            title=s_month + ' Archive',
+            month=month,
+            fn_out=fn_out,
+            path=path,
+            posts=month_posts,
+            count=len(month_posts)))
+        archive.append(archive_data)
+    return archive
 
 class Object(dict):
 
